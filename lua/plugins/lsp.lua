01cdb5493c9c9fd96945565eda7ec79e14e3cc07
JeanProvost <jprov310@gmail.com>
2026-04-25 12:13:10 -0600

    lsp changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -83,7 +83,8 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
-      'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
+      { 'L3MON4D3/LuaSnip', version = "v2.*" },
+      'saadparwaiz1/cmp_luasnip',
     },
     config = function()
       local cmp = require('cmp')
